e5da533b38ee00cc1ab87fea483b629326d85bc4
Shubhankar Tripathi <shubhankar9817@gmail.com>
2021-12-17 20:29:10 +0530

    commiting problem number 952

--- DSU/952. Largest Component Size by Common Factor.cpp
+++ DSU/952. Largest Component Size by Common Factor.cpp
@@ -53,90 +53,29 @@ public:
     }
 };
 
-class primeFactorization{
-    vi spf;
-public:
-    primeFactorization(int n)
-    {
-        int i;
-        spf.resize(n);
-        
-        for(i=0;i<n;i++)
-        {
-            spf[i]=0;
-        }
-    }
-    
-    void sieve()
-    {
-        spf[1] = 1;
-
-        for (int i=2; i<100001; i++)
-            spf[i] = i;
-
-        for (int i=4; i<100001; i+=2)
-            spf[i] = 2;
-
-        for (int i=3; i*i<100001; i++)
-        {
-            if (spf[i] == i)
-            {
-                for (int j=i*i; j<100001; j+=i)
-                    if (spf[j]==j)
-                        spf[j] = i;
-            }
-        }
-    }
-
-    vector<int> getFactorization(int x)
-    {
-        vector<int> ret;
-
-        while (x != 1)
-        {
-            ret.push_back(spf[x]);
-            x = x / spf[x];
-        }
-
-        return ret;
-    }
-};
-
 class Solution {
 public:
     int largestComponentSize(vector<int>& nums) {
         union_find DSU(100001);
-        primeFactorization pf(100001);
-        pf.sieve();
-        vector<int> adj[100001];
-        int i,j,size;
+        int i,j,size,ans=0;
+        unordered_map<int,int> m;
         
         for(auto &x:nums)
         {
-            vector<int> factors=pf.getFactorization(x);
-            for(auto &y:factors)
+            for(i=2;i*i<=x;i++)
             {
-                adj[y].push_back(x);
+                if(x%i==0)
+                {
+                    DSU.un(x,i);
+                    DSU.un(x,x/i);
+                }
             }
         }
         
-        for(i=1;i<=100000;i++)
+        for(auto &x:nums)
         {
-            vi temp=adj[i];
-            size=temp.size();
-            
-            for(j=0;j<size-1;j++)
-            {
-                DSU.un(temp[j],temp[j+1]);
-            }
-        }
-        
-        int ans=0;
-        vi sizes = DSU.getSize();
-        
-        for(i=1;i<=100000;i++)
-        {
-            ans=max(ans,sizes[i]);
+            // ++m[DSU.root(x)];
+            ans=max(ans,++m[DSU.root(x)]);
         }
         
         return ans;
