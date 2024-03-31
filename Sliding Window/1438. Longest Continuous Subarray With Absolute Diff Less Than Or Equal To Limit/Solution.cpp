acb4d5311c64eed6b834b7d292d9fcb3569f44ff
AmpleCoder <shubhankar9817@gmail.com>
2024-04-01 03:54:00 +0530

    longest subarray where the difference between the max and min value is <= limit or where all pair diff <= limit

---
+++ Sliding Window/1438. Longest Continuous Subarray With Absolute Diff Less Than Or Equal To Limit/Solution.cpp
@@ -0,0 +1,27 @@
+/*
+    approach1 :- 
+        as we know if in a subarray the diff between the max and min val is <= limit then each and every pair will 
+        satisfy the same condition hence we can use multiset to store to fetch the max and min and keep on popping ele 
+        which are of no use for use from start 
+*/
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        multiset<int> m;
+        int size = nums.size(), start = 0, maxSize = 1;
+
+        for(int end = 0; end < size; end++){
+            m.insert(nums[end]);
+
+            while(start <= end && (*(m.rbegin()) - *(m.begin()) > limit)){
+                m.erase(m.find(nums[start]));
+                start++;
+            }
+
+            maxSize = max(maxSize, end - start + 1);
+        }
+        
+        return maxSize;
+    }
+};
